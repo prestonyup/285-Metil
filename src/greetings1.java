a7c06c696c5a975194928be73630c390160748da
ferusutajp <prestonmetil@gmail.com>
2026-09-17 09:09:44 -0400

    a

---
+++ src/greetings1.java
@@ -0,0 +1,10 @@
+public class greetings1
+{
+    public static void main(String [] args)
+    {
+        String firstName = "Preston";
+        String lastname = "Metil";
+        System.out.println("Hello, " + firstName + " " + lastname);
+        System.out.println("Kaboom, you did it");
+    }
+}
