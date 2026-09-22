b609c5cead09c83b673821baa64da3269fd98e1e
ferusutajp <prestonmetil@gmail.com>
2026-09-22 09:21:20 -0400

    pythag

---
+++ src/pythagPGM.java
@@ -0,0 +1,18 @@
+// (1) Input all three sides
+// (2) Determine that two sides squared equal the largest side
+// (3) Largest side has to be less than the sum of the others
+// (4) Output the answer:
+    // Is a right triangle
+    // Is a triangle, but not right
+    // Is not a triangle
+
+import java.util.Scanner;
+
+public class pythagPGM {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+
+    }
+
+}
